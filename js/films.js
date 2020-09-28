da169eca47744d7dc8016010109f4da223fcf33f
gallienne <antoine.gallienne@free.fr>
2020-09-28 19:56:16 +0200

    class +tableau

--- js/films.js
+++ js/films.js
@@ -28,15 +28,63 @@ let episodetitle = episode.title;
 let episodeduration = episode.duration;
 let episodehasBeenWatched = episode.hasBeenWatched;
 
-
-
 // =========================================
 
 document.querySelector('#episode-info').innerText = `Episode: ${episodeTitle}
 Duration: ${episodeDuration} min
 ${hasBeenWatched ? 'Already watched' : 'Not yet watched'}`
 
+// ==========================================
+// class
 
+class Episode {
+    constructor(title,duration,hasBeenWatched) {
+        this.title = title;
+        this.duration = duration;
+        this.hasBeenWatched = hasBeenWatched;
+    }
+}
+
+
+let firstEpisode = new Episode("episode 1", 45, true);
+let secondEpisode = new Episode("episode 2", 45, true);
+let thirdEpisode = new Episode("episode 3", 45, false);
+// =====================================
+
+document.querySelector('#first-episode-info').innerText = `Episode: ${firstEpisode.title}
+Duration: ${firstEpisode.duration} min
+${firstEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+
+document.querySelector('#second-episode-info').innerText = `Episode: ${secondEpisode.title}
+Duration: ${secondEpisode.duration} min
+${secondEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+
+document.querySelector('#third-episode-info').innerText = `Episode: ${thirdEpisode.title}
+Duration: ${thirdEpisode.duration} min
+${thirdEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+
+// array
+// ====================================
+
+let episodes = [firstEpisode, secondEpisode, thirdEpisode];
+
+// ====================================
+
+const body = document.querySelector('body');
+
+for(let i = 0; i < 12; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('series-frame');
+    let newTitle = document.createElement('h2');
+    newTitle.innerText = 'The Story of Tau';
+    let newParagraph = document.createElement('p');
+    newParagraph.innerText = `${episodes[i].title}
+${episodes[i].duration} minutes
+${episodes[i].hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+    newDiv.append(newTitle);
+    newDiv.append(newParagraph);
+    body.append(newDiv);
+}
 
 // Constante
 // =====================================
